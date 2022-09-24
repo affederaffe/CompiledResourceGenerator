633bcf180a9941f3ed56f761e5ff2609cfb9567b
affederaffe <68356204+affederaffe@users.noreply.github.com>
2022-09-24 13:02:13 +0200

    Add benchmark

---
+++ CompiledResourceGenerator.Benchmarks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace CompiledResourceGenerator.Benchmarks
+{
+     internal static partial class Program
+     {
+          public static void Main() => BenchmarkRunner.Run<Test>();
+
+
+          [CompiledResource("LoremIpsum.txt")]
+          public static partial byte[] GetBytes();
+     }
+
+     [MemoryDiagnoser]
+     public class Test
+     {
+          [Benchmark]
+          public void EmbeddedResourceStreamBenchmark()
+          {
+               Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CompiledResourceGenerator.Benchmarks.LoremIpsum.txt")!;
+               new StreamReader(stream).ReadToEnd();
+          }
+
+          [Benchmark]
+          public void CompiledResourceBenchmark()
+          {
+               Stream stream = new MemoryStream(Program.GetBytes());
+               new StreamReader(stream).ReadToEnd();
+          }
+     }
+}
